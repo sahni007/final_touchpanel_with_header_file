989ee5ce01cec69054db689ca844fc8e6f3b4da8
Varun Sahni <42349105+sahni007@users.noreply.github.com>
2018-11-28 22:27:41 +0530

    Update actiontouchpanel.h

--- actiontouchpanel.h
+++ actiontouchpanel.h
@@ -1,7 +1,10 @@
+
+  
 void actiontouchPanel(char Switch_Num, char sw_status, char Sw_speed) //, char speeds
 {
 
-    M1=ON;M2=ON;M3=ON;M4=ON; 
+     M1=ON;M2=ON;M3=ON;M4=ON;  M5=ON; M6=ON; M7=ON; M8=ON;
+
     int switch_status = sw_status - '0';        
     int SwNum = Switch_Num - '@';//ASCII OF SWITCH NUMBER - ASCII OF @ i.e A>>65, B>>66, C>>67, D>>68 65-64=1 and so on
     int int_swSpeed = Sw_speed-'0';
@@ -14,7 +17,7 @@ void actiontouchPanel(char Switch_Num, char sw_status, char Sw_speed) //, char s
  }   
     else
     {
- //    TX1REG='T';
+
         switch(Switch_Num)
         {
 
@@ -23,7 +26,7 @@ void actiontouchPanel(char Switch_Num, char sw_status, char Sw_speed) //, char s
                if(copy_parentalLockBuffer[1] == CHAR_OFF && M1 == ON)
                   {
                        sendFeedback_TO_Gateway('1',sw_status);
-                       OUTPUT_RELAY4 = switch_status; M1 = OFF;
+                       RELAY1 = switch_status; M1 = OFF;
 
                   }
                }
@@ -35,13 +38,14 @@ void actiontouchPanel(char Switch_Num, char sw_status, char Sw_speed) //, char s
                  if( copy_parentalLockBuffer[2] == CHAR_OFF && M2 == ON  )
                   {       
                         sendFeedback_TO_Gateway('2',sw_status);
-                         OUTPUT_DIMMER =  ~switch_status;  M2 = OFF;
+                         RELAY2 = switch_status;  M2 = OFF;
 
                   }
 
                }
 
                break;
+
              default:
              break;
         }
