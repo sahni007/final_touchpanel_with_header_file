f4a6ef6fbccef3a7ab28a90baab9b200b78410ec
Varun Sahni <42349105+sahni007@users.noreply.github.com>
2018-11-28 21:58:41 +0530

    update

--- application.h
+++ application.h
@@ -1,4 +1,7 @@
 
+
+
+
 void applianceControl(char charSwitchMSB, char charSwitchLSB, char charSwitchSTATE, char chDimmerSpeedMSB, char chDimmerSpeedLSB,
         char charParentalControl, char charFinalFrameState){
     
@@ -12,7 +15,7 @@ void applianceControl(char charSwitchMSB, char charSwitchLSB, char charSwitchSTA
     //define all used character data types and initlize it with "#"
     char switchNumberStringBuffer[2]="#";
     char dimmerSpeedStringBuffer[2]="#";
-
+    
     switchNumberStringBuffer[0]=charSwitchMSB;
     switchNumberStringBuffer[1]=charSwitchLSB;    
     integerSwitchNumber = atoi(switchNumberStringBuffer);//convert string into integer
@@ -20,8 +23,7 @@ void applianceControl(char charSwitchMSB, char charSwitchLSB, char charSwitchSTA
     // Get switch State in Integer Format
     
     integerSwitchState = charSwitchSTATE-'0';
-//    TX1REG=chDimmerSpeedMSB;
-//    TX1REG=chDimmerSpeedLSB;
+    
     // Get speed of Fan or level of dimmer    
     dimmerSpeedStringBuffer[0]=chDimmerSpeedMSB;
     dimmerSpeedStringBuffer[1]=chDimmerSpeedLSB;    
@@ -49,7 +51,6 @@ void applianceControl(char charSwitchMSB, char charSwitchLSB, char charSwitchSTA
     if(charFinalFrameState=='1')    // until 
     {
         sendAcknowledgment(currentStateBuffer+currentStateBufferPositions);  
-        if(integerSwitchNumber!=5){
         __delay_ms(5);
         TX2REG = '(' ;
         __delay_ms(1);
@@ -66,141 +67,63 @@ void applianceControl(char charSwitchMSB, char charSwitchLSB, char charSwitchSTA
         TX2REG='0';
         __delay_ms(1);
         TX2REG=')';
-        }
-        else if(integerSwitchNumber==5) {
-           switch(integerSwitchState){
-            case 0: {
-                    send_Response_To_Touch('P','0','0');  
-                   } break;
-           
-            case 1: {
-                switch(chDimmerSpeedMSB){
-                    case '0':
-                    {
-                    send_Response_To_Touch('P','1','1'); //SPEED1
-                    }
-                    break;
-                    case '2':
-                    {
-                      send_Response_To_Touch('P','1','1');  //SPEED1
-                    }
-                    break;
-                    case '5':
-                    {
-                      send_Response_To_Touch('P','1','2'); //SPEED2
-                    }
-                    break;
-                    case '7':
-                    {
-                      send_Response_To_Touch('P','1','3');  //SPEED3
-                    }
-                    break;
-                    case '9':
-                    {
-                      send_Response_To_Touch('P','1','4'); //SPEED4
-                
-                    }
-                    break;
-                    default:
-                    break;
-            }//close of switch
-        }
-      }
     }
-  }
     
     switch(integerSwitchNumber){
         case 1:
         {
-             RELAY1 = integerSwitchState;
+
+
+             OUTPUT_RELAY1 = integerSwitchState;
+
+
         }
             break;
         case 2:
             {
-              RELAY2 = integerSwitchState;
+
+//            TX1REG='2';
+
+              OUTPUT_RELAY2 = integerSwitchState;
 
             break;
             }
         case 3:
         {
-            RELAY3 = integerSwitchState;
+        
+//            TX1REG='3';
+           
+            OUTPUT_RELAY3 = integerSwitchState;
+
+
         }
             break;
         case 4:
         {
-            RELAY4 = integerSwitchState;
+//            TX1REG='4';
+          
+            OUTPUT_RELAY4 = integerSwitchState;
+
         }
             break;
         case 5:
         {
-        if(integerSwitchState==0){  
-                
-                FAN1=OFF;
-                __delay_ms(1000);
-                FAN2=OFF;
-                __delay_ms(1000);
-                FAN3=OFF;
-                 }
-        else if(integerSwitchState==1)
-                    {  
-               
-                       if(chDimmerSpeedMSB == '0')             // speed 1
-                        {
-                                 
-                                  FAN3=OFF;
-                                  __delay_ms(1000);
-                                  FAN2=OFF;
-                                  __delay_ms(1000);
-                                  FAN1=ON;
-
-                          }
-                        else if(chDimmerSpeedMSB == '2')             // speed 1
-                        {
-                                  
-                                  FAN3=OFF;
-                                  __delay_ms(1000);
-                                  FAN2=OFF;
-                                  __delay_ms(1000);
-                                  FAN1=ON;
-
-                          }
-                       else if(chDimmerSpeedMSB == '5')         // speed 2
-                              {
-                                  
-                                  FAN1=OFF;
-                                  __delay_ms(1000);
-                                  FAN3=OFF;               
-                                  __delay_ms(1000);
-                                  FAN2=ON;
-                              }
-
-                          else if( chDimmerSpeedMSB=='7')      // speed 3
-                              {    
-                                  
-                                  FAN3=OFF;
-                                  __delay_ms(1000);
-                                  FAN2=ON;
-                                  __delay_ms(1000);
-                                  FAN1=ON;
-                              }
-                      else if( chDimmerSpeedMSB == '9')                         // speed 4
-                              {   
-
-                                  
-                                  REGULATOR=ON; 
-                                  __delay_ms(1000);
-                                  FAN1=OFF;//OFF;
-                                  __delay_ms(1000);
-                                  FAN2=OFF;//OFF;
-                                 __delay_ms(1000);
-                                  FAN3=ON;
-                              }
-                     }       
-
+            
+                OUTPUT_RELAY5 = integerSwitchState;
         }
             break;
+            
+        case 6:
+        {
+                OUTPUT_RELAY6 = integerSwitchState;
+        }
+            break;
+
         default:
             break;
         }
     
 }
+
+
+
